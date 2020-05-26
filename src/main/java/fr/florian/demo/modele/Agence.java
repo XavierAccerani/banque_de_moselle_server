08c5288133b9a8247aea548b63d87b233b46980d
shayann <shayann@kernel>
2020-05-27 00:36:48 +0200

    added class of agance to modele

--- src/main/java/fr/florian/demo/modele/Agence.java
+++ src/main/java/fr/florian/demo/modele/Agence.java
@@ -1,12 +1,66 @@
 package fr.florian.demo.modele;
 
+import javax.persistence.*;
+
+@Entity
 public class Agence {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column
+    private  boolean actif;
+
+    @Column
     private String nom;
 
+    @Column
     private String adresse;
 
-    private String type_agence;
+    @Column
+    private String typeAgence;
+
+    public Agence() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getTypeAgence() {
+        return typeAgence;
+    }
+
+    public void setTypeAgence(String typeAgence) {
+        this.typeAgence = typeAgence;
+    }
+
+    public boolean isActif() {
+        return actif;
+    }
+
+    public void setActif(boolean actif) {
+        this.actif = actif;
+    }
 }

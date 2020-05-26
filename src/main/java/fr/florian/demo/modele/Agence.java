abd36a8866416a203b152b71619f1f4a8180dfa6
matthieutno <matthieu.tragno6@etu.univ-lorraine.fr>
2020-05-26 17:07:22 +0200

    Ajout de la classe "Agence" et des classes afférentes.

---
+++ src/main/java/fr/florian/demo/modele/Agence.java
@@ -0,0 +1,50 @@
+package fr.florian.demo.modele;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class Agence {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private boolean actif;
+
+    @Column
+    private String nom;
+
+    @Column
+    private String adresse;
+
+    @Column
+    private String typeAgence;
+
+    public Agence() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public boolean isActif() { return actif; }
+
+    public void setActif(final boolean actif) { this.actif = actif; }
+
+    public String getNom() { return nom; }
+
+    public void setNom(String nom) { this.nom = nom; }
+
+    public String getAdresse() { return adresse; }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getTypeAgence() {
+        return typeAgence;
+    }
+
+    public void setTypeAgence(String typeAgence) {
+        this.typeAgence = typeAgence;
+    }
+}

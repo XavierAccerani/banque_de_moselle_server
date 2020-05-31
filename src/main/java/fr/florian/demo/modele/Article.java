35c23648dce07ef9e5b8465ea90176947bbea5c9
matthieutno <matthieu.tragno6@etu.univ-lorraine.fr>
2020-05-31 15:30:47 +0200

    Création de la classe "Article" dans le modèle

---
+++ src/main/java/fr/florian/demo/modele/Article.java
@@ -0,0 +1,60 @@
+package fr.florian.demo.modele;
+
+import javax.persistence.*;
+
+@Entity
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private boolean actif;
+
+    @Column
+    private String designation;
+
+    @Column
+    private double prixUnitaire;
+
+    @Column
+    private double tauxTva;
+
+    public Article() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public boolean isActif() {
+        return actif;
+    }
+
+    public void setActif(boolean actif) {
+        this.actif = actif;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public double getPrixUnitaire() {
+        return prixUnitaire;
+    }
+
+    public void setPrixUnitaire(double prixUnitaire) {
+        this.prixUnitaire = prixUnitaire;
+    }
+
+    public double getTauxTva() {
+        return tauxTva;
+    }
+
+    public void setTauxTva(double tauxTva) {
+        this.tauxTva = tauxTva;
+    }
+}
